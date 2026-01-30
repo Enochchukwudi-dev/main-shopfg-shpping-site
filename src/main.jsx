1ab024d6b7d46dc5d30ea96af72910cf9f220f40
enoch <enoch2@gmail.com>
2026-01-30 17:04:33 +0100

    Update shopfg project

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
 import { createRoot } from "react-dom/client";
-import { HashRouter as Router } from "react-router-dom";   // ← changed from BrowserRouter
+import { HashRouter as Router } from "react-router-dom";
 import "./index.css";
-import App from "./App.jsx";
-import ScrollToTop from "./components/ScrollToTop";
-import WhatsAppFloat from "./components/WhatsAppFloat";
-import { CartProvider } from './context/CartContext';
+import lockImg from "./assets/lock.png";
 
 createRoot(document.getElementById("root")).render(
   <Router>
-    <ScrollToTop />
-    <CartProvider>
-      <App />
-    </CartProvider>
-    <WhatsAppFloat />
+    <div className="lock-viewport" role="img" aria-label="Website locked">
+      <img src={lockImg} alt="Website locked" />
+    </div>
   </Router>
 );
+
