0eea61a7ab983fdd6a9c1cc14f20eac0dab96313
Enochchukwudi-dev <your-email@example.com>
2026-02-05 13:39:32 +0100

    Update homepage

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,27 @@
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import ScrollToTop from "./components/ScrollToTop";
-import WhatsAppFloat from "./components/WhatsAppFloat";
-import { CartProvider } from "./context/CartContext";
 
-createRoot(document.getElementById("root")).render(
-  <Router>
-    <ScrollToTop />
-    <CartProvider>
-      <App />
-    </CartProvider>
-    <WhatsAppFloat />
-  </Router>
-);
+import { CartProvider } from "./context/CartContext";
+import Countdown from "./components/Countdown";
+
+function Root() {
+  const [showApp, setShowApp] = useState(false);
+
+  return (
+    <Router>
+      <ScrollToTop />
+      <CartProvider>
+        {/* Countdown will show until 7:30 PM local time. When it finishes it calls onFinish and we show the app (routes). */}
+        <Countdown onFinish={() => setShowApp(true)} />
+        {showApp ? <App /> : null}
+      </CartProvider>
+  
+    </Router>
+  );
+}
+
+createRoot(document.getElementById("root")).render(<Root />);
